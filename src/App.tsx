efaeac2122fb3682afaa9d6dc18fff0a00fc864f
Pierre-Etienne Soury <pierretienne.soury@gmail.com>
2023-05-03 15:45:01 +0200

    infra: init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="section yellow" />
+        <div className="section pink" />
+        <div className="section orange" />
+      </div>
+    </>
+  );
+}
+
+export default App;
